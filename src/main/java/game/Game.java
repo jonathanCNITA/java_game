36a3b48b829cd36990b01fe7729f71626903d36b
Jonathan Courat <jonathan.courat@campus-numerique-in-the-alps.com>
2018-03-20 11:57:06 +0100

    Convert the player array to  an ArrayList & more options

--- src/main/java/game/Game.java
+++ src/main/java/game/Game.java
@@ -1,25 +1,29 @@
 package game;
 
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Game {
     public static void main(String[] args) {
         
         Scanner scannerInput = new Scanner(System.in);
-        Personnage[] player = new Personnage[3];
+        ArrayList<Personnage> player = new ArrayList<Personnage>();
         String userChoice;
-        Boolean custom = true;
 
-        for( int i = 0; i < player.length; i++) {
+        Boolean custom = true;
+        int itemNum = 0;
+
+        while(true) {
             custom = true; 
-            System.out.println("Choisissez un personnage !" +"(" + i + ")");
+            System.out.println("Choisissez un personnage !" +"(" + itemNum + ")");
             System.out.println("1 -Warrior 2 -Magician");
             userChoice = scannerInput.nextLine();
+
             if(userChoice.equals("1") ) {
-                player[i] = new Warrior();
+                player.add(new Warrior());
                 System.out.println("new warrior created.");
             } else {
-                player[i] = new Magician();
+                player.add(new Magician());
                 System.out.println("new magician created.");
             }
             while(custom){
@@ -28,25 +32,25 @@ public class Game {
                 userChoice = scannerInput.nextLine();
             
                 if(userChoice.equals("1")){
-                    System.out.println(player[i].toString());
+                    System.out.println(player.get(itemNum).toString());
                 } else if (userChoice.equals("2")) {
                     System.out.println("Choisissez un nom:");
                     userChoice = scannerInput.nextLine();
-                    player[i].setName(userChoice);
+                    player.get(itemNum).setName(userChoice);
             
                     System.out.println("Choisissez une image (lien Url):");
                     userChoice = scannerInput.nextLine();
-                    player[i].setImg(userChoice);
+                    player.get(itemNum).setImg(userChoice);
             
                     System.out.println("Choisissez votre niveau de vie:");
                     userChoice = scannerInput.nextLine();
-                    player[i].setLife(Integer.parseInt(userChoice));
+                    player.get(itemNum).setLife(Integer.parseInt(userChoice));
             
                     System.out.println("Choisissez votre force d'attaque:");
                     userChoice = scannerInput.nextLine();
-                    player[i].setAttack(Integer.parseInt(userChoice));
+                    player.get(itemNum).setAttack(Integer.parseInt(userChoice));
 
-                    System.out.println(player[i].toString());
+                    System.out.println(player.get(itemNum).toString());
                 } else if(userChoice.equals("3")) {
                     System.out.println("J'ai pas compris!");
                 }  else if (userChoice.equals("4")) {
@@ -54,23 +58,24 @@ public class Game {
                     String weaponName = scannerInput.nextLine();
                     System.out.println("Choisissez sa force:");
                     int weaponForce = Integer.parseInt(scannerInput.nextLine());
-                    player[i].addWeapon(weaponName, weaponForce);
-                    System.out.println(player[i].toString());
+                    player.get(itemNum).addWeapon(weaponName, weaponForce);
+                    System.out.println(player.get(itemNum).toString());
                 }
 
-                System.out.println("Continuer ? (exit pour quitter)");
+                System.out.println("1-Creer un autre personnage 2-Voir la liste des personnages 3-Editer ce personnage");
                 userChoice = scannerInput.nextLine();
-                if(userChoice.equals("exit")) {
+                if(userChoice.equals("1")) {
+                    itemNum++;
                     custom = false;
+                } else if(userChoice.equals("2")){
+                    System.out.println("//////////////////////");
+                    for(int perso = 0; perso < player.size(); perso++){
+                        System.out.println((player.get(perso)).toString());
+                        System.out.println("-------------------------");
+                    }
+                    System.out.println("//////////////////////");
                 }
             }
-
-        }
-            
-        for(int i = 0; i < 3; i++){
-            System.out.println(player[i].toString());
-        }     
-
-        System.out.println("exit the game");
+        } 
     }
 }
